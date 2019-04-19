61cda27024ec8700a44018430f0471d09aaa15d5
Dries Vints <dries.vints@gmail.com>
2019-04-19 13:42:10 +0000

    Apply fixes from StyleCI

--- tests/Unit/ImpersonatesUsersTest.php
+++ tests/Unit/ImpersonatesUsersTest.php
@@ -2,9 +2,9 @@
 
 namespace Laravel\BrowserKitTesting\Tests\Unit;
 
-use Laravel\BrowserKitTesting\Concerns\ImpersonatesUsers;
 use Laravel\BrowserKitTesting\Tests\TestCase;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Laravel\BrowserKitTesting\Concerns\ImpersonatesUsers;
 
 class ImpersonatesUsersTest extends TestCase
 {
@@ -16,18 +16,43 @@ class ImpersonatesUsersTest extends TestCase
     public function set_currently_logged_in_user_for_app()
     {
         $user = new class implements Authenticatable {
-            public function getAuthIdentifierName() {}
-            public function getAuthIdentifier() {}
-            public function getAuthPassword() {}
-            public function getRememberToken() {}
-            public function setRememberToken($value) {}
-            public function getRememberTokenName() {}
+            public function getAuthIdentifierName()
+            {
+            }
+
+            public function getAuthIdentifier()
+            {
+            }
+
+            public function getAuthPassword()
+            {
+            }
+
+            public function getRememberToken()
+            {
+            }
+
+            public function setRememberToken($value)
+            {
+            }
+
+            public function getRememberTokenName()
+            {
+            }
         };
 
         $this->app['auth'] = new class {
             public $user;
-            public function guard() { return $this; }
-            public function setUser($user) { return $this->user = $user; }
+
+            public function guard()
+            {
+                return $this;
+            }
+
+            public function setUser($user)
+            {
+                return $this->user = $user;
+            }
         };
 
         $this->be($user);
